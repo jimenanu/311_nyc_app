725ef105dfe20f3a282b39b07d9cabf20083a8ed
jimenanu <JimenaNavarro2026@u.northwestern.edu>
2026-04-24 17:22:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ for feature in geojson["features"]:
     feature["properties"]["color"] = [247, 227, 0, min(255, int(val / 50000))]
 
 #  LOGO
-st.image("T51-NB2.png", width=280)
+st.image("T51-NB2.png", width=700)
 
 # HEADER
 st.title("NYC 311 Analytics Tracker")
